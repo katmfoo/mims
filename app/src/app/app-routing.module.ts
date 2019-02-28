deb43c0a4d8dab3e97ffd87e45d815fb1d8298cf
dmarandino <43444397+dmarandino@users.noreply.github.com>
2019-02-28 15:46:05 -0500

    Fixed Search Page

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login-page', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login-page', loadChildren: './login-page/login-page.module#LoginPagePageModule' },
+  { path: 'search', loadChildren: './search/search.module#SearchPageModule' },
 ];
 
 @NgModule({
