acff2fc84697104f171ea021b34cd9575c4885ee
pricheal <patrickricheal@gmail.com>
2019-03-01 16:09:44 -0500

    Add basic search functionality to app

--- app/src/app/home/home.page.ts
+++ app/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiCallService } from '../services/api-call.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  public items: Array<any> = [];
+  public search_term: string;
+
+  constructor(private apiCall: ApiCallService) {}
+
+  search() {
+    if (this.search_term) {
+      this.apiCall.get('/products/', {search_term: this.search_term}).then((response: any) => {
+        if (response.success) {
+          this.items = response.data.items;
+        }
+      });
+    }
+  }
 }
