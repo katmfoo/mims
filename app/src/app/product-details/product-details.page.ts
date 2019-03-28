e111fdc9fa838964c7a1f1a67d6c859df2603da2
finnion285 <43827744+finnion285@users.noreply.github.com>
2019-03-28 17:30:52 -0400

    Updated product details html

--- app/src/app/product-details/product-details.page.ts
+++ app/src/app/product-details/product-details.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ApiCallService } from '../services/api-call.service';
 
 @Component({
   selector: 'app-product-details',
@@ -8,13 +9,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsPage implements OnInit {
   public item_code;
+  public item;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private apiCall: ApiCallService) { }
 
   ngOnInit() {
     this.item_code = this.route.snapshot.params.itemCode;
     console.log(this.item_code);
+
+      if (this.item_code) {
+        this.apiCall.get('/products/' + this.item_code + '/', {}).then((response: any) => {
+          if (response.success) {
+            this.item = response.data.product;
+            console.log(this.item);
+          }
+        });
+      }
+      
+    
   }
+
+
 
 }
 
