342160a66c42e4a1cd0761d26535bc538e54ae5d
pricheal <patrickricheal@gmail.com>
2019-04-01 10:09:40 -0400

    Fix back button for product forecast page

--- app/src/app/product-forecast/product-forecast.page.ts
+++ app/src/app/product-forecast/product-forecast.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-forecast',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductForecastPage implements OnInit {
 
-  constructor() { }
+  public item_code;
+
+  constructor(private navCtrl: NavController, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.item_code = this.route.snapshot.params.itemCode;
+  }
+
+  goBack()
+  {
+    this.navCtrl.navigateBack('product-details' + '/' + this.item_code);
   }
 
 }
