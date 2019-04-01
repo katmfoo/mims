31480fbc3a0b451e4eef9451a46837672907f557
pricheal <patrickricheal@gmail.com>
2019-04-01 10:28:09 -0400

    Fix movement page

--- app/src/app/product-movement/product-movement.page.ts
+++ app/src/app/product-movement/product-movement.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiCallService } from '../services/api-call.service';
-import { utils } from 'protractor';
+import * as moment from 'moment'
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -12,18 +12,13 @@ import { NavController } from '@ionic/angular';
 export class ProductMovementPage implements OnInit {
   public item_code;
   public item;
-  public movement;
-  public movementKeys: Array<any> = [];
-  public movementValues: Array<any> = [];
-  public movementDays: Array<any> = [];
+
+  public movement = [];
 
   constructor(private route: ActivatedRoute, private apiCall: ApiCallService, private navCtrl: NavController) { }
 
   ngOnInit() {
-
     this.item_code = this.route.snapshot.params.itemCode;
-    console.log(this.item_code);
-
     this.downloadMovement();
     this.downloadProduct();
   }
@@ -32,16 +27,15 @@ export class ProductMovementPage implements OnInit {
   downloadMovement() {
     this.apiCall.get('/products/' + this.item_code + '/movement/', {}).then((response: any) => {
       if (response.success) {
-        this.movement = response.data.product_movement;
-        this.movementKeys = Object.keys(this.movement);
-        this.movementValues = Object.values(this.movement);
-        this.movementKeys = this.movementKeys.slice(this.movementKeys.length - 7, this.movementKeys.length + 1);
-        this.movementValues = this.movementValues.slice(this.movementValues.length - 7, this.movementValues.length + 1);
-        this.toDayofWeek();
-        }
-        
 
-      
+        for (let item in response.data.product_movement) {
+          this.movement.unshift({
+            'date': moment(item),
+            'inventory_amount': response.data.product_movement[item]
+          });
+        }
+
+      }
     });
   }
 
@@ -54,17 +48,7 @@ export class ProductMovementPage implements OnInit {
     });
   }
 
-  toDayofWeek() {
-    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    for (let i = 0; i < this.movementKeys.length; i++) {
-    var d = new Date(this.movementKeys[i]);
-    this.movementDays[i] = days[d.getDay()];
-    }
-  }
-
-  goBack()
-  {
+  goBack() {
     this.navCtrl.navigateBack('product-details' + '/' + this.item_code);
   }
-
 }
