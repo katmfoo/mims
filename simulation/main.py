3a54f0f71b670814c0bacb2a97c455f6b898540d
Steven Bruman <steven.bruman@yahoo.com>
2019-05-06 10:38:33 -0400

    Ordering items uses forecast data && documentation

--- simulation/main.py
+++ simulation/main.py
@@ -37,9 +37,7 @@ def main():
         simtime = datetime.today() - timedelta(days=365)
         initialInventory(headers, items, (str(simtime.date()) + " " + str(simtime.time().isoformat(timespec='seconds'))))
 
-        """Sets when the store is 
-            either opened or closed
-            """
+        # A 24x7 2D array detailing when the store is open
         openclose = [[False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False],
                      [False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False],
                      [False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False],
@@ -48,6 +46,7 @@ def main():
                      [False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False],
                      [False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, False, False]]
 
+        # A 24x7 2D array detailing wait times at the store.
         waitTimes = [[1,1,1,1,1,1,1,5,5,5,10,10,10,15,15,15,10,10,10,10,10,10,5,5,1],
                      [1,1,1,1,1,1,1,5,5,5,10,10,10,15,15,15,15,15,15,10,10,10,10,5,1],
                      [1,1,1,1,1,1,1,5,10,10,10,10,10,15,15,15,15,15,15,10,10,5,5,1],
@@ -58,25 +57,31 @@ def main():
 
         print(str(simtime.date()) + " " + str(simtime.time().isoformat(timespec='seconds')))
 
+        # Historical Sales Generation
         wait = False
         waitTime = 0
         sales = 0
         last_time = simtime
         while(simtime < datetime.today()):
+            # Checks to see if it is a new week and changes prices
             if(simtime.weekday() == 6 and simtime.weekday() - last_time.weekday() == 1):
                 changePrices(headers, items, (str(simtime.date()) + " " + str(simtime.time().isoformat(timespec='seconds'))))
+            # Checks to see if it is a day and orders new inventory
             if (last_time.weekday() - simtime.weekday() != 0):
                 newInventory(headers, items, (str(simtime.date()) + " " + str(simtime.time().isoformat(timespec='seconds'))))
+            # Are we currently waiting for a new sale
             if(waitTime > 0):
                 waitTime = waitTime - 1
             elif(waitTime==0):
                 if(wait):
+                    # New Sale
                     makeSale(headers, items, (str(simtime.date()) + " " + str(simtime.time().isoformat(timespec='seconds'))))
                     print (str(simtime.date()) + " " + str(simtime.time().isoformat(timespec='seconds')))
                     sales = sales + 1
                 wait = False
                 if(openclose[simtime.weekday()][simtime.hour]):
                     wait = True
+                    # Determines new wait time
                     waitTime = random.randint(5,(600/waitTimes[simtime.weekday()][simtime.hour]))
             last_time = simtime
             simtime = simtime + timedelta(minutes=1)
@@ -84,20 +89,26 @@ def main():
 
         currentDT = datetime.now()
 
+        # Real Time sales generation
         while(True):
             time.sleep(waitTime * 60)
             last_time = currentDT
             currentDT = datetime.now()
+            # Are we currently waiting for a new sale
             if(waitTime > 0):
+                # Checks to see if it is a new week and changes prices
                 if (currentDT.weekday() == 6 and currentDT.weekday() - last_time.weekday() == 1):
                     changePrices(headers, items, (str(currentDT.date()) + " " + str(currentDT.time().isoformat(timespec='seconds'))))
+                # Checks to see if it is a day and orders new inventory
                 if (last_time.weekday() - currentDT.weekday() != 0):
                     newInventory(headers, items, (str(currentDT.date()) + " " + str(currentDT.time().isoformat(timespec='seconds'))))
+                # New Sale
                 makeSale(headers, items, str(currentDT.date()) + " " + str(currentDT.time().isoformat(timespec='seconds')))
                 print(str(currentDT.date()) + " " + str(currentDT.time().isoformat(timespec='seconds')))
                 sales = sales + 1
             if (openclose[currentDT.weekday()][currentDT.hour]):
                 wait = True
+                # Determines new wait time
                 waitTime = random.randint(5, (600/waitTimes[currentDT.weekday()][currentDT.hour]))
             else:
                 waitTime = 0
@@ -107,7 +118,9 @@ def main():
 
 def makeSale(headers, items, simtime):
     shopperType = random.randint(1,100)
+    # List of items in the sale
     sale = []
+    # Light Shopper
     if(shopperType < 60):
         numItems = random.randint(1,3)
         for i in range(1,numItems):
@@ -135,6 +148,7 @@ def makeSale(headers, items, simtime):
                 elif (inventory > 0):
                     amount = random.randint(1, inventory)
                 if(amount > 0):
+                    # Format for a sale item
                     newItem = {
                         "item_code": item,
                         "amount": amount,
@@ -143,6 +157,7 @@ def makeSale(headers, items, simtime):
                     sale.append(newItem)
             except json.decoder.JSONDecodeError:
                 print("Couldn't Get Amount Of Item")
+    # Medium Shopper
     elif(shopperType < 90):
         numItems = random.randint(2, 5)
         for i in range(1, numItems):
@@ -172,6 +187,7 @@ def makeSale(headers, items, simtime):
                 elif (inventory > 0):
                     amount = random.randint(1, inventory)
                 if (amount > 0):
+                    # Format for a sale item
                     newItem = {
                         "item_code": item,
                         "amount": amount,
@@ -180,6 +196,7 @@ def makeSale(headers, items, simtime):
                     sale.append(newItem)
             except json.decoder.JSONDecodeError:
                 print("Couldn't Get Amount Of Item")
+    # Heavy Shopper
     else:
         numItems = random.randint(5, 10)
         for i in range(1, numItems):
@@ -207,6 +224,7 @@ def makeSale(headers, items, simtime):
                 elif (inventory > 5):
                     amount = random.randint(1, int(inventory / 5))
                 if (amount > 0):
+                    # Format for a sale item
                     newItem = {
                         "item_code": item,
                         "amount": amount,
@@ -245,8 +263,37 @@ def newInventory(headers, items, simtime):
             product = data["product"]
             inventory = product["current_inventory"]
             per = product["units_per_case"]
-            if inventory < per * 10:
-                amount = random.randint(1,10)
+            # Getting forecast data
+            url += "forecast/"
+            payload = ""
+            x = requests.request("GET", url, data=payload, headers=headers)
+            print(x.text)
+            response = x.json()
+            print(response["data"])
+            data = response["data"]
+            forecast = data["product_forecast"]
+            today_forecast = forecast[str(simtime.date)]
+            # Using the forecast to predict how much inventory they will need
+            # This is an optimistic store
+            if inventory < int(today_forecast * 1.5):
+                diff = int(today_forecast * 1.5) - inventory
+                amount = diff / per
+                data = {
+                    "item_code": item,
+                    "amount": amount,
+                    "unit": 3,
+                    "datetime": simtime
+                }
+                payload = json.dumps(data)
+                print(data)
+                try:
+                    url = "http://ec2-54-81-254-121.compute-1.amazonaws.com:5000/inventory/"
+                    x = requests.request("POST", url, data=payload, headers=headers)
+                except json.decoder.JSONDecodeError:
+                    print("Couldn't Make New Inventory")
+            # If the forecast or inventory was 0 then something may have gone wrong and they still need to order
+            elif today_forecast == 0 or inventory == 0:
+                amount = random.randint(1,3)
                 data = {
                     "item_code": item,
                     "amount": amount,
@@ -283,6 +330,7 @@ def initialInventory(headers, items, simtime):
 
 
 def changePrices(headers, items, simtime):
+    # 10 random items will increase their price by 10%
     for i in range(1,10):
         try:
             url = "http://ec2-54-81-254-121.compute-1.amazonaws.com:5000/products/"
@@ -313,6 +361,7 @@ def changePrices(headers, items, simtime):
                 print("Couldn't Set New Price")
         except json.decoder.JSONDecodeError:
             print("Couldn't Get Price Of Item")
+    # 10 randon items will drop their price by 10%
     for i in range(1,10):
         try:
             url = "http://ec2-54-81-254-121.compute-1.amazonaws.com:5000/products/"
