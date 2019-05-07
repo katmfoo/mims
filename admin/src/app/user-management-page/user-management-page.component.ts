ab541e5f484cafdefe5a402fc6e221d408dfc88c
Sagarika K <kumars52@students.rowan.edu>
2019-05-07 00:56:50 -0400

    added comments to user management ts

--- admin/src/app/user-management-page/user-management-page.component.ts
+++ admin/src/app/user-management-page/user-management-page.component.ts
@@ -1,3 +1,4 @@
+//imports
 import { Component, OnInit } from '@angular/core';
 import { ApiCallService } from '../services/api-call.service';
 import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
@@ -12,58 +13,58 @@ export class UserManagementPageComponent implements OnInit {
 
   public users: Array<any> = [];
   public data: any = {page_size: 10, page: 1};
-  public search_term: string;
+  public search_term: string;  //search users on user management page
   public selectedUser: string; // used for opening the edit modal
 
   constructor(private apiCall: ApiCallService, private modalService: NgbModal, private router: Router) {}
 
   ngOnInit() {
-    this.updateUsers();
-    this.setUpUsername();
+    this.updateUsers(); //update users after changes
+    this.setUpUsername(); 
   }
 
   logout() {
     localStorage.removeItem('access-token');
-    this.router.navigate(['login']);
+    this.router.navigate(['login']); //navigate to login after calling logout function
   }
 
-  updateUsers() {
-    this.apiCall.get('/users/', this.data).then((response: any) => {
+  updateUsers() {  //any changes on users
+    this.apiCall.get('/users/', this.data).then((response: any) => {  //api call 
       if (response.success) {
         this.users = response.data.users;
       }
     });
   }
 
-  searchUsers() {
-    if (this.search_term) {
+  searchUsers() {   //see if search term appears in data
+    if (this.search_term) { 
       this.data.page = 1;
       this.data.search_term = this.search_term;
     } else {
-      delete this.data.search_term;
+      delete this.data.search_term; 
     }
     this.updateUsers();
   }
 
-  nextPage() {
+  nextPage() {  //go to the next page once next page button is pressed
     if (!this.nextDisabled()) {
       this.data.page += 1;
       this.updateUsers();
     }
   }
 
-  previousPage() {
+  previousPage() {    // go to previous page once previous page button is pressed
     if (!this.previousDisabled()) {
       this.data.page -= 1;
       this.updateUsers();
     }
   }
 
-  nextDisabled() {
+  nextDisabled() {    //disable next page button if the page isn't filled
     return this.users.length < this.data.page_size;
   }
 
-  previousDisabled() {
+  previousDisabled() {  //disable previous page if theres no previous page
     return this.data.page <= 1;
   }
 
@@ -81,9 +82,10 @@ export class UserManagementPageComponent implements OnInit {
     this.updateUsers();
   }
 
-  openEditModal(content, username : string) {
+  openEditModal(content, username : string) {  //open edit modal after edit is clicked
     this.selectedUser = username;
     var edit_permission : Boolean = true;
+    //check if a user is about to edit their own info and give them a warning
     if (username.toLowerCase() == localStorage.getItem('current-username').toLowerCase()) {
       edit_permission = confirm("You are about to edit your own user information. Do you wish to continue?"
         + "\nNote: editing your own information may cause issues with your permission on this page.");
@@ -94,11 +96,13 @@ export class UserManagementPageComponent implements OnInit {
   }
 
   deleteUser(user_id, username : string) {
+    //delete a user. Checks to see if the user is trying to delete their own account and doesn't let them
     if (username.toLowerCase() == localStorage.getItem('current-username').toLowerCase()) {
       alert("Unable to delete user.This user is currently logged in.");
       return;
     }
     else {
+      //or lets them delete a user after confirmation
       var response = confirm("Are you sure you want to delete user: " + username + "?");
       if (response) {
         this.apiCall.put('/users/' + user_id + '/', {
