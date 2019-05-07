3bd780eb32cccf6994b28e6f24c71bce38e50732
Sagarika K <kumars52@students.rowan.edu>
2019-05-07 00:15:21 -0400

    added comments on login page.ts

--- admin/src/app/login-page/login-page.component.ts
+++ admin/src/app/login-page/login-page.component.ts
@@ -1,3 +1,4 @@
+//imports
 import { Component, OnInit } from '@angular/core';
 import { ApiCallService } from '../services/api-call.service';
 import { Router } from '@angular/router';
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
-  public username: string;
+  public username: string;  
   public password: string;
   public errorMsg: string;
 
@@ -28,22 +29,22 @@ export class LoginPageComponent implements OnInit {
     // Reset error message
     this.errorMsg = "";
 
-    // Ensure both fields are filled out
+    // Ensure both fields are filled out when loggin in
     if (!this.username || !this.password) {
       this.errorMsg = "All fields required";
       return;
     } else {
-      this.apiCall.post('/login/', {
+      this.apiCall.post('/login/', {   //call login api
         username: this.username,
         password: this.password
       }).then((response: any) => {
         if (response.success) {
-          if (response.data.user_type == 1) {
+          if (response.data.user_type == 1) {    //no employees allowed to login
             localStorage.setItem('access-token', response.data.access_token);
             localStorage.setItem('current-username', this.username);
             this.router.navigate(['users']);
           } else {
-            this.errorMsg = 'Must be manager to login';
+            this.errorMsg = 'Must be manager to login';   //if employee tries to login
             return;
           }
         } else {
